26fa1c34054e15a734b991765b794a7402556d6d
Deepa Gupta <130374750+Deepa-2705@users.noreply.github.com>
2023-06-28 20:43:49 +0530

    Add files via upload

---
+++ Arrays/Assignment-3/BinarySearch.java
@@ -0,0 +1,28 @@
+package Assignment_3;
+//Question 4
+//Given a sorted array of distinct integers and a target value, return the index if the target is found.
+// If not, return the index where it would be if it were inserted in order.
+//You must write an algorithm with O(log n) runtime complexity.
+//Input: nums = [1,3,5,6], target = 5
+//Output: 2
+
+class BinarySearch{
+    public static void main(String[] args) {
+        int[] nums={1,3,5,6};
+        int target=5;
+        System.out.println("Element found at index: "+searchInsert(nums,target));
+    }
+    public static int searchInsert(int[] nums, int target) {
+        int start = 0;
+        int end = nums.length-1;
+
+        while (start <= end) {
+            int mid = start + (end-start)/2;
+            if (nums[mid] == target) return mid;
+            else if (nums[mid] > target) end = mid-1;
+            else start = mid+1;
+        }
+
+        return start;
+    }
+}
